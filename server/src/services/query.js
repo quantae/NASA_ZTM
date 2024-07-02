0f377970fee27cfac26fdeeab6c42a84fd4ba718
Gerald O-A, quantae <onegerrald@gmail.com>
2024-07-02 20:43:48 +0000

    Added GitHub Actions workflow for Docker

--- server/src/services/query.js
+++ server/src/services/query.js
@@ -6,7 +6,7 @@ const DEFAULT_PAGE_LIMIT = 10;
 function getPagination(query) {
   const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
   const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
-  const skip = (page - 1) * limit;
+  const skip = (page) * limit;
 
   return {
     // we return the variables as an object.
