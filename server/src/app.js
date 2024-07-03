955f5c1f30caf7804761b013ef419e3c8b96c632
Gerald O-A, quantae <onegerrald@gmail.com>
2024-07-03 10:05:39 +0000

    Added font scr

--- server/src/app.js
+++ server/src/app.js
@@ -20,9 +20,10 @@ app.use((req, res, next) => {
     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
     res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     res.setHeader('Origin-Agent-Cluster', '?1');
-    res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src 'self' https://64.227.46.35:8000 https://happyhealth.greenjadefarms.com:8000");
+    res.setHeader("Content-Security-Policy", "default-src 'self' https://64.227.46.35:8000 https://happyhealth.greenjadefarms.com:8000; connect-src 'self' https://64.227.46.35:8000 https://happyhealth.greenjadefarms.com:8000; style-src 'self' 'https://fonts.googleapis.com' 'unsafe-inline'; font-src 'self' 'https://fonts.gstatic.com'; img-src 'self' data:;");
     next();
-  });
+});
+
 app.use(express.json());
 app.use(morgan('combined'));
 
